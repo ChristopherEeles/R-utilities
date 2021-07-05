d68986425df5cca4d8756d7caf25b6e53eaa357d
ChristopherEeles <christopher.eeles@outlook.com>
2021-07-04 20:11:07 -0400

    Update: Add CLI parsing to select R version to compile in compile_R.sh

--- compile_R.sh
+++ compile_R.sh
@@ -1,4 +1,47 @@
 #!/bin/bash
+
+# -- make sure we are on an OS with apt
+if [[ ! "$OSTYPE" =~ linux.* ]]
+then
+    echo "This script requires the apt utility to work correctly!"
+    exit 1
+fi
+
+# -- parse command line args
+SOURCE='https://cran.r-project.org/src/base/R-latest.tar.gz'
+VERSION='latest'
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -v|--version)
+            VERSION="$2"
+            case $VERSION in
+                devel)
+                    SOURCE='https://stat.ethz.ch/R/daily/R-devel.tar.gz'
+                    ;;
+                patched)
+                    SOURCE='https://stat.ethz.ch/R/daily/R-patched.tar.gz'
+                    ;;
+                *)
+                    echo 'Valid options for the -v/--version parameter are ' \
+                        '\n"devel" for the development version or "patched" ' \
+                        '\nfor the previous release. If you want R "release",' \
+                        '\n exclude the -v/--version parameter since that is' \
+                        'the default for this script.'
+                    ;;
+            esac
+            shift # past argument
+            shift # past value
+            ;;
+        *)
+            echo "The only valid option is for this script is -v/--version."
+            exit 1
+            ;;
+    esac
+done
+
+# -- install R compilation dependencies
+## TODO: Generalize this to work with other installation utilties (brew, yum)
 echo "Installing required dependencies"
 sudo apt -y install \
     build-essential\
@@ -16,37 +59,47 @@ sudo apt -y install \
     libpcre++-dev \
     libpango1.0-dev \
     libpango1.0-dev \
-    libpcre2-dev
-    tk-dev
-    tk-table
+    libpcre2-dev \
+    tk-dev \
+    tk-table \
     tcl-dev
 echo "Dependencies installed!"
-echo "Downloading R-devel source code..."
-wget https://stat.ethz.ch/R/daily/R-devel.tar.gz
-tar -xvf R*tar.gz
+
+# -- download the correct R version
+echo "Downloading R-$VERSION source code..."
+wget $SOURCE
+tar -xvf R-"$VERSION"*tar.gz --one-top-level=R-"$VERSION" --strip-components=1
 echo "Done"
-echo "Switching into R-devel directory..."
-cd R-devel
-echo "In `pwd`!"
-R_VERSION=devel
+
+# -- configure and make the R source files
+echo "Switching into R-$VERSION directory..."
+cd "R-$VERSION" || exit
+echo "In $(pwd)!"
 echo "Configuring installation..."
 ./configure \
-    --prefix=/opt/R/$R_VERSION \
+    --prefix=/opt/R/R-"$VERSION" \
     --enable-memory-profiling \
     --enable-R-shlib \
     --with-tcltk \
     --with-blas \
     --with-lapack
-echo "Making R-devel..."
+echo "Making R-$VERSION..."
 make
+
+# -- install the R source files
 echo "Installing from source..."
 sudo make install
 echo "Done"
+
+# -- update the system symlinks to the version just installed
 echo "Creating symlinks"
 sudo rm /usr/local/bin/R /usr/local/bin/Rscript
-sudo ln -s /opt/R/${R_VERSION}/bin/R /usr/local/bin/R
-sudo ln -s /opt/R/${R_VERSION}/bin/Rscript /usr/local/bin/Rscript
+sudo ln -s /opt/R/R-"$VERSION"/bin/R /usr/local/bin/R
+sudo ln -s /opt/R/R-"$VERSION"/bin/Rscript /usr/local/bin/Rscript
 echo "Done!"
+
+# -- check if the installation succeeded
 echo "Testing installation was succesful:"
-/opt/R/${R_VERSION}/bin/R --version
+/opt/R/R-"$VERSION"/bin/R --version
 echo "Successful is above returned a version number!"
+exit 0
