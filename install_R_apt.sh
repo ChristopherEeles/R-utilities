77a60130e0326b4cc79ec1a35578ffbbdc32a43c
ChristopherEeles <christopher.eeles@outlook.com>
2022-05-01 21:59:56 -0400

    Add lsb-release and lsb-core install to install_R_apt.sh

--- install_R_apt.sh
+++ install_R_apt.sh
@@ -4,7 +4,7 @@
 sudo apt-get update -y && sudo apt-get upgrade -y
 
 # -- Add R repo to apt, then use it to install R
-sudo apt install dirmngr gnupg apt-transport-https ca-certificates \
+sudo apt install -y dirmngr gnupg apt-transport-https ca-certificates \
     software-properties-common
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
     E298A3A825C0D65DFD57CBB651716619E084DAB9
@@ -15,9 +15,9 @@ sudo add-apt-repository \
 apt_pkgs=(
     # system deps
     'libcurl4-openssl-dev' 'libssl-dev' 'libxml2-dev' 'build-essentials'
-    'libsodium-dev' 'libopenblas-dev'
+    'libsodium-dev' 'libopenblas-dev' 'lsb-release' 'lsb-core'
     # databases
-    'sqlite' 'mariabd-server-10.3' 'mariadb-server' 
+    'sqlite' 'mariabd-server-10.3' 'mariadb-server'
     'libmariadbclient-dev' 'mysql-server'
     # CLI tools
     'git'  'pigz'  'htop'
